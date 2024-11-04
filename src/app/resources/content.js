7eedc68d1a7827d305a47446fada7d64fb66c2a8
Muhammad Shozab Raza <razashozab619@gmail.com>
2024-11-04 19:18:51 +0500

    34

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -234,7 +234,7 @@ const about = {
 
 const blog = {
     label: 'Blog',
-    title: 'Writing about design and tech...',
+    title: 'Writing about web3 and thier derivatives...',
     description: `Read what ${person.name} has been up to recently`
     // Create new blog posts by adding a new .mdx file to app/blog/posts
     // All posts will be listed on the /blog route
@@ -243,7 +243,7 @@ const blog = {
 const work = {
     label: 'Work',
     title: 'My projects',
-    description: `Design and dev projects by ${person.name}`
+    description: `Designs and Community projects by ${person.name}`
     // Create new project pages by adding a new .mdx file to app/blog/posts
     // All projects will be listed on the /home and /work routes
 }
