35aea63f340c98eeaf0d9fb8ae8fb5255e657403
Muhammad Shozab Raza <razashozab619@gmail.com>
2024-11-06 19:31:49 +0500

    Update content.js

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -64,7 +64,7 @@ const about = {
     },
     calendar: {
         display: true,
-        link: 'https://cal.com'
+        link: 'https://cal.com/shozabraza/30min'
     },
     intro: {
         display: true,
